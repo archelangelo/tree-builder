526d80862e4ac322c053ca73095b88915322470d
Yiran Hu <medycc@gmail.com>
2019-09-14 19:52:25 -0400

    Added some structure to work on.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,22 @@
-var worldSize, nodeSize, nodePadding, levelHeight;
+var nodeSize, nodePadding, levelHeight;
 var root;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+    nodeSize = 10.;
+    nodePadding = 4.;
+    levelHeight = nodeSize + 2*nodePadding;
+    root = new Node(windowWidth/2., nodeSize/2. + nodePadding, null);
+    root.left = new Node(root.x - (nodeSize + nodePadding), root.y + levelHeight, root);
+    root.right = new Node(root.x + (nodeSize + nodePadding), root.y + levelHeight, root);
+    myCamera = new Camera();
+}
+
+function draw() {
+    background(255);
+    root.display();
+}
 
 class Node {
     constructor(x, y, parent) {
@@ -12,6 +29,7 @@ class Node {
         this.width = nodeSize;
     }
 
+    // Display the tree staring from this node with dfs
     display() {
         if (this.left != null) {
             this.left.display();
@@ -24,21 +42,32 @@ class Node {
         }
         ellipse(this.x, this.y, nodeSize, nodeSize);
     }
+
+    // Calculate and adjust the sizes and positions of this node and the subtree under it
+    // Returns a number indicating the width of the subtree
+    adjustPosition(baseX, depth, rightBound) {
+        // TODO: perform dfs to accumulation the width of the the entire subtree and update the postions of the nodes
+        return this.width;
+    }
+
+    // Click trigger
+    onClick() {
+        // TODO: prompts the user to set the value, add children or delete
+    }
 }
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(255);
-    worldSize = 1.;
-    nodeSize = 10.;
-    nodePadding = 4.;
-    levelHeight = nodeSize + 2*nodePadding;
-    root = new Node(windowWidth/2., nodeSize/2. + nodePadding, null);
-    root.left = new Node(root.x - (nodeSize + nodePadding), root.y + levelHeight, root);
-    root.right = new Node(root.x + (nodeSize + nodePadding), root.y + levelHeight, root);
-}
+// Draw objects through the lens of this camera
+// Support zoom and automatically adjust the size
+class Camera {
+    constructor() {
+        this.x = 0.;
+        this.y = 0.;
+        this.scale = 1.;
+    }
 
-function draw() {
-    background(255);
-    root.display();
+    // Draw an ellipse
+    ellipse(x, y, w, h) {}
+
+    // Draw a line
+    line(x1, y1, x2, y2) {}
 }
